e959dd6780328ddee82ea902da5aec483e698cee
MT <mat.pxo@gmail.com>
2020-09-02 12:14:52 -0300

    Expres, nunjuvks e afins

--- app.js
+++ app.js
@@ -2,9 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const {indexHome} = require('./pages')
+
+const nunjucks = require('nunjucks')
+
+nunjucks.configure('src/views', {
+    express: app,
+    noCache: true,
 })
+
+
+app
+
+.use(express.urlencoded({ extended: true}))
+.use(express.static("public"))
+.get("/", indexHome)
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
